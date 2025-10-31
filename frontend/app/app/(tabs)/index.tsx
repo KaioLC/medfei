4e2fb030e6718bf2bd69ee6d18ea76348034a8c4
KaioLC <kaiolopessilva@gmail.com>
2025-10-31 03:45:42 -0300

    enviando requirements do python + ajustes no .gitignore

--- frontend/app/app/(tabs)/index.tsx
+++ frontend/app/app/(tabs)/index.tsx
@@ -2,13 +2,30 @@ import { StyleSheet, Text, View, Button, TextInput, Alert, ScrollView, SafeAreaV
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Constants from 'expo-constants'; // tira o hardcode do IP
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
-// pega o IP dinamicamente
-const hostUri = Constants.expoConfig?.extra?.hostUri;
+// pegando o IP dinamicamente
 
-const hostname = hostUri ? hostUri.split(':')[0] : 'localhost';
+function getApiHostname() {
 
+  // pega o hostUri das variaveis de ambiente do expo
+  const hostUri = Constants.expoConfig?.extra?.hostUri;
+  // separa o hostname da porta
+  if(hostUri) {
+    return hostUri.split(':')[0];
+  }
+  // pega o linkingUri das variaveis de ambiente do expo
+  const linkingUri = Constants.linkingUri;
+  if(linkingUri) {
+    // separa o hostname da porta
+    return linkingUri.replace('exp://', '').split(':')[0];
+  }
 
+  console.warn("Não foi possível detectar o IP do host, usando localhost");
+  return 'localhost';
+}
+
+const hostname = getApiHostname();
 const API_URL =  `http://${hostname}:5000`; // porta do backend flask;
 
 console.log(`Conectando ao backend em: ${API_URL}`); // debugando o URL do back
